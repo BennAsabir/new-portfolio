65b0d47368d413cd5216ecce7407d88cf90dbf06
Benzino <benasabir@gmail.com>
2021-03-28 08:41:12 -0400

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,11 @@ function toggleFunction() {
     } else {
         x.className = x.className.replace(" w3-show", "");
     }
+}                  
+
+// When the user clicks on div, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
 }
+
